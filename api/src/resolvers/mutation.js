9447e6c6ffb324ccf36aa2fa977698aff6ccf831
Mario Bischoff <mariobischoff@hotmail.com>
2021-02-13 18:24:56 -0300

    :alien: [API] First commit

---
+++ api/src/resolvers/mutation.js
@@ -0,0 +1,77 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError,
+  ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar');
+
+module.exports = {
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLowerCase();
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed
+      });
+      return await jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }]
+    });
+    if (!user) {
+      throw new AuthenticationError('Error signing in');
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError('Error signing in');
+    }
+    return await jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  },
+  newNote: async (parent, args, { models }) => {
+    return await models.Note.create({
+      content: args.content,
+      author: 'Mario Bischoff'
+    });
+  },
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  updateNote: async (parent, { content, id }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
+  }
+};
+
+
+// pag 86
